dd5a5d93d5e7d30716f8f104947291f92296a461
ADMIN <ADMIN@PC230>
2025-05-15 17:00:48 +0700

    View, ADD

---
+++ WebBanHang/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hosting;
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment hosting)
+        {
+            _db = db;
+            _hosting = hosting;
+        }
+        //Hiển thị danh sách sản phẩm
+        public IActionResult Index()
+        {
+            var productList = _db.Products.Include(x => x.Category).ToList();
+            return View(productList);
+        }
+
+        public IActionResult Add()
+        {
+            //truyền danh sách thể loại cho View để sinh ra điều khiển DropDownList
+            ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Name
+            });
+            return View();
+        }
+
+    }
+}
